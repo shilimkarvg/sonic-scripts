71d8a014ac20bb00500eb30c4ed8e086e84b749c
Sabareesh Kumar Anandan <sanandan@marvell.com>
2020-06-08 13:15:59 +0530

    inband mgmt update

--- sonic_M0_master_mvl_inband_mgmt_patch.sh
+++ sonic_M0_master_mvl_inband_mgmt_patch.sh
@@ -83,6 +83,8 @@ done
 sed -i 's/sleep 1/sleep 4/g' Makefile.work
 
 # WA to restart networking for inband mgmt
+sed -i '$ a inbandMgmtPortNum=47' device/marvell/armhf-marvell_et6448m_52x-r0/et6448m/profile.ini
+
 sed -i '/build_version/i \
 /bin/sh /etc/inband_mgmt' files/image_config/platform/rc.local
 
@@ -92,12 +94,7 @@ sudo cp $IMAGE_CONFIGS/platform/inband_mgmt $FILESYSTEM_ROOT/etc/' files/build_t
 rm -f files/image_config/platform/inband_mgmt
 echo "#inband_mgmt" > files/image_config/platform/inband_mgmt
 sed -i '$ a \
-cat /usr/share/sonic/device/armhf-marvell_et6448m_52x-r0/et6448m/profile.ini | grep inbandMgmtPortNum 2> /dev/null\
-if [ $? -ne 0 ]; then\
-echo "Using OOB"\
-exit 0\
-fi\
-echo "Using Inband Mgmt"\
+sleep 60
 inband_mgmt(){\
  while :; do\
    ip -br link show eth0 2> /dev/null\
